1db9eced1fffcf0c7e0e2f6686d8bd894154cb5c
Evtr0na <3133861288@qq.com>
2026-06-10 12:55:51 +0800

    测试

--- scene/level.gd
+++ scene/level.gd
@@ -1,10 +1,4 @@
 extends Node3D
-@onready var character_body_3d: CharacterBody3D = $Player/CharacterBody3D
-@onready var character_body_3d_2: CharacterBody3D = $Player/CharacterBody3D2
-@onready var free_camera:Node3D = $Player/camera
-@onready var cam_01: Camera3D = $Player/CharacterBody3D/SpringArm3D/Camera3D
-@onready var cam_02: Camera3D = $Player/CharacterBody3D2/SpringArm3D/Camera3D
-@onready var cam_03: Camera3D = $Player/camera/SpringArm3D/Camera3D
 
 
 
